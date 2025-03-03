6ae5f1df72cb2136d92f7153c817f4f47d711030
TomTyack <thomas.tyack@conversiondigital.com.au>
2025-03-03 16:09:53 +1100

    sanity initial

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,36 @@ import { renderPage } from "./renderPageContents"
 import { fetchPageData, fetchMetadata, logPrefix } from "@conversiondigital/headless-basics-data";
 import { getLogger } from "@conversiondigital/headless-basics-data";
 
-import { PageBlueprint, PageDefinition } from "@conversiondigital/headless-basics-data/src/interfaces";
+import { PageBlueprint } from "@conversiondigital/headless-basics-data/src/interfaces";
 
 const log = getLogger("page.Homepage")
 
 export default async function Page() {
-  log.info(`${logPrefix()}[SHOWCASE] Page Homepage loading -- ${process.env.SITE_ID} -- ${process.env.UMBRACO_API_KEY}`) 
+  log.info(`${logPrefix()}[SHOWCASE] Page Homepage loading -- ${process.env.SITE_ID}`) 
   setupSite();
   log.trace(`${logPrefix()}[SHOWCASE] Page Homepage loading`)
   const bluePrint: PageBlueprint = await fetchPageData({ slug: ["/"], source: "page.Homepage.fetchPageData" })
-  log.info(`${logPrefix()}[SHOWCASE] Page Homepage loaded ${process.env.SITE_ID}`)
+  
+  // log.info(`${logPrefix()}[SHOWCASE] Page Homepage loaded ${process.env.SITE_ID}`)
+  // await import(`@conversiondigital/headless-basics-components/src/theme/default/components/subcomponentcontent`)
+  // await import(`@conversiondigital/headless-basics-components/src/theme/default/components/subcomponentcontent/sanity-mapping`)
+  // const identifier = "subcomponentcontent";
+  // const cmsPrefix = "heartcore";
+  // const cmsPrefix2 = "sanity";
+
+  // // await import(
+  // //   `@conversiondigital/headless-basics-components/src/theme/default/components/${identifier}/${cmsPrefix}-mapping`
+  // // )
+
+  // // await import(`@conversiondigital/headless-basics-components/src/theme/default/components/${identifier}/${cmsPrefix}-mapping`);
+  // // await import(`@conversiondigital/headless-basics-components/src/theme/default/components/${identifier}/${cmsPrefix2}-mapping`);
+  // await import(`@conversiondigital/headless-basics-components/src/theme/default/components/subcomponentcontent`).then((module) => module.default);
+  // return (<> Hello </>)
   return renderPage(bluePrint)
 }
 
-export async function generateMetadata({ params }) {
-  setupSite();
-  log.trace(`${logPrefix()}[SHOWCASE] generateMetadata for homepage`)
-  return await fetchMetadata({ slug: ["/"] })
-}
+// export async function generateMetadata({ params }) {
+//   setupSite();
+//   log.trace(`${logPrefix()}[SHOWCASE] generateMetadata for homepage`)
+//   return await fetchMetadata({ slug: ["/"] })
+// }
